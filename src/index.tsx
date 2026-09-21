2aefb90288963142dc5670d419ce8b10fb96e388
FlameWolf <agnijith@gmail.com>
2026-09-21 16:50:33 +0530

    Change app root element name from `app` to `root`

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import { createComponent } from "solid-js";
 import { render } from "@solidjs/web";
 import App from "@/App";
 
-const root = document.getElementById("app");
+const root = document.getElementById("root");
 
 if (!root) {
-	throw new Error('Root element "#app" was not found.');
+	throw new Error(`Root element "#root" was not found.`);
 }
 
 render(() => createComponent(App, {}), root);
