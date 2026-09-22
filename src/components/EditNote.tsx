c5b123cf74558b2e704c18be0efe32bc9e97c3dc
FlameWolf <agnijith@gmail.com>
2026-09-22 15:43:02 +0530

    Prevent confirm dialogue box from being shown twice

--- src/components/EditNote.tsx
+++ src/components/EditNote.tsx
@@ -161,7 +161,9 @@ export default function EditNote(props: Props) {
 		}
 		if (isCreateMode()) {
 			setIsEditing(false);
-			navigate(backRoute());
+			queueMicrotask(() => {
+				navigate(backRoute());
+			});
 		} else {
 			const note = existingNote();
 			if (note) {
